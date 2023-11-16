29c88dda7a4d53c3310b286b6f0689df1c133e4c
MarceloWalther <marcelo.walther@compasso.com.br>
2023-11-16 19:19:43 -0300

    create anime controller

--- src/main/java/academy/devdojo/DemoApplication.java
+++ src/main/java/academy/devdojo/DemoApplication.java
@@ -2,8 +2,10 @@ package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"academy.devdojo", "teste.api"})  //basePackages pacote base, com o @ComponentScan eu consigo dizer para o spring monitorar e gerenciar outros pacotes
 public class DemoApplication {
 
 	public static void main(String[] args) {
