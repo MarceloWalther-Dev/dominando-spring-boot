60344c370c955626f5e38232a7cb05ba231f9cac
MarceloWalther <marcelo.walther@compasso.com.br>
2023-11-30 13:20:39 -0300

    trabalhando com requestParam

--- src/main/java/academy/devdojo/controller/CustomerController.java
+++ src/main/java/academy/devdojo/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package academy.devdojo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,11 @@ public class CustomerController {
         //http://localhost:8080/v1/customers/filterList?name=Marcelo,Arthur
     }
 
-
+    @GetMapping("{name}")
+    public String findByName(@PathVariable String name){
+        return NAMES.stream()
+                .filter(n -> n.equalsIgnoreCase(name))
+                .findFirst()
+                .orElseGet(() -> "Nome não encontrado");
+    }
 }
