2195146ef70d0a503e03bcb26a22f7a4d4e59299
MarceloWalther <marcelo.walther@compasso.com.br>
2023-12-01 11:43:49 -0300

    desafio post

--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -3,28 +3,46 @@ package academy.devdojo.controller;
 import academy.devdojo.domain.Anime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping(path = {"/v1/animes", "/v1/animes/"})
 public class AnimeController {
 
+
     @GetMapping
     public List<Anime> list() {
-        return Anime.animesList();
+        return Anime.getAnimesList();
     }
 
     @GetMapping("filterName")
     public Anime findByName(@RequestParam(value = "name") String name) {
-        return Anime.animesList().stream().filter(anime -> anime.getName().equalsIgnoreCase(name)).findFirst().orElse(new Anime());
+        return Anime.getAnimesList().stream().filter(anime -> anime.getName().equals(name))
+                .findFirst().orElse(null);
     }
 
     @GetMapping("/{id}")
     public Anime findById(@PathVariable(value = "id") Long id) {
-        return Anime.animesList().stream().filter(anime -> anime.getId().equals(id)).findFirst().orElse(new Anime());
+        return Anime.getAnimesList()
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    @PostMapping
+    public Anime save(@RequestBody Anime animeRequest){
+        animeRequest.setId(ThreadLocalRandom.current().nextLong(100_000));
+        Anime.getAnimesList().add(animeRequest);
+        System.out.println(Anime.getAnimesList());
+        return animeRequest;
     }
 }
--- src/main/java/academy/devdojo/domain/Anime.java
+++ src/main/java/academy/devdojo/domain/Anime.java
@@ -2,24 +2,33 @@ package academy.devdojo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
+@ToString
 public class Anime {
 
     private Long id;
 
     private String name;
 
-    public static List<Anime> animesList(){
+    private static List<Anime> animesList = new ArrayList<Anime>();
+
+    static {
         var anime1 = new Anime(1L, "Naruto");
         var anime2 = new Anime(2L, "Dragon Ball");
         var anime3 = new Anime(3L, "Pokemon");
         var anime4 = new Anime(4L, "Shurato");
-        return List.of(anime1, anime2, anime3, anime4);
+        animesList.addAll(List.of(anime1, anime2, anime3, anime4));
+    }
+
+    public static List<Anime> getAnimesList() {
+        return animesList;
     }
 }
